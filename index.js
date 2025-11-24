2c2e33be17ab5b3162363a88d1228ed8d241a64e
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-11-24 10:43:00 -0300

    feat: add route to delete a task

--- index.js
+++ index.js
@@ -33,6 +33,26 @@ app.post('/tasks', async (req, res) => {
 
 })
 
+app.delete('/tasks/:id', async (req, res) => { 
+    try {
+        const taskId = req.params.id;
+
+        const taskToDelete = await TaskModel.findById(taskId);
+
+        if (!taskToDelete) {
+            return res.status(500).send('Task not found.')
+        }
+
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+        res.status(200).send(deletedTask);
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+
+
+})
+
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
 });
